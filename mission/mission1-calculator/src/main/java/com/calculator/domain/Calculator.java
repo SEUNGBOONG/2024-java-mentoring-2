3987e1feedc094106ce5566edda94f9553a9eb4b
승엽 <tmdduqflfl@naver.com>
2024-12-28 19:57:03 +0900

    Feat: 마이너스 메서드 구현

--- mission/mission1-calculator/src/main/java/com/calculator/domain/Calculator.java
+++ mission/mission1-calculator/src/main/java/com/calculator/domain/Calculator.java
@@ -2,7 +2,11 @@ package com.calculator.domain;
 
 public class Calculator {
 
-    public int addCal(int x, int y){
-        return x+y;
+    public int addCal(int x, int y) {
+        return x + y;
+    }
+
+    public int minusCal(int x, int y) {
+        return x - y;
     }
 }
